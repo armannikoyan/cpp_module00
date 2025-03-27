481989f053a5a4f3f450bf4dd0bc43c9aef8f43c
Arman Nikoyan <anikoyan@c2r10s4.42yerevan.am>
2025-03-27 20:21:29 +0400

    done: ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char **argv) {
+  if (argc <= 1) {
+    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    return (0);
+  }
+  for (int row = 1; row < argc; ++row)
+  {
+    std::string line(argv[row]);
+    for (ssize_t i = 0; i < line.length(); ++i)
+      line[i] = std::toupper(line[i]);
+    std::cout << line;
+  }
+  std::cout << std::endl;
+  return (0);
+}
